89da5f89ab0cc511e88246bc990ab92848149251
Dmitrii Galanov <dgalanov@luxoft.corp-partner.google.com>
2019-12-07 23:08:03 +0300

    Attempted to solve threeSum

--- java/src/com/company/ThreeSum.java
+++ java/src/com/company/ThreeSum.java
@@ -1,37 +1,34 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.HashSet;
+import java.util.*;
 
 class ThreeSum {
-    List<List<Integer>> threeSum(int[] nums) {
-        HashSet< List<Integer> > result = new HashSet<>();
-        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                Integer value = - (nums[i] + nums[j]);
-                if (map.containsKey(value)) {
-                    var list = map.get(value);
-                    list.add(nums[i]);
-                }
-                else
-                    map.put(value, new HashSet<>(List.of(nums[i])));
+    List< List<Integer> > twoSum(int[] nums, int i) {
+        int target = -nums[i];
+        HashSet<Integer> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        for (int pos = 0; pos < nums.length; ++pos) {
+            if (pos == i) continue;
+            int x = nums[pos];
+            int y = target - x;
+            if (set.contains(x)) {
+                List<Integer> list = new ArrayList<>(Arrays.asList(
+                        x, y, -target));
+                Collections.sort(list);
+                result.add(list);
+            }
+            else {
+                set.add(y);
             }
         }
 
-        for (int num: nums) {
-            if (map.containsKey(num)) {
-                var ingredients = map.get(num);
-                for (var ingredient: ingredients) {
-                    ArrayList<Integer> resultPart = new ArrayList<>();
-                    resultPart.add(num);
-                    resultPart.add(ingredient);
-                    resultPart.add(-(ingredient + num));
-                    result.add(resultPart);
-                }
-            }
+        return result;
+    }
+
+    List<List<Integer>> threeSum(int[] nums) {
+        HashSet< List<Integer> > result = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            result.addAll(twoSum(nums, i));
         }
 
         return new ArrayList<>(result);
