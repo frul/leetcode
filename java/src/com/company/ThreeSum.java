4c24579ddc67f11c8528c10942508a0970793615
Dmitrii Galanov <dgalanov@luxoft.corp-partner.google.com>
2019-12-08 17:41:55 +0300

    Attempted to solve threeSum [2]

--- java/src/com/company/ThreeSum.java
+++ java/src/com/company/ThreeSum.java
@@ -1,34 +1,61 @@
 package com.company;
 
+import javafx.util.Pair;
+
 import java.util.*;
 
 class ThreeSum {
-    List< List<Integer> > twoSum(int[] nums, int i) {
-        int target = -nums[i];
-        HashSet<Integer> set = new HashSet<>();
-        List<List<Integer>> result = new ArrayList<>();
-        for (int pos = 0; pos < nums.length; ++pos) {
-            if (pos == i) continue;
-            int x = nums[pos];
-            int y = target - x;
-            if (set.contains(x)) {
-                List<Integer> list = new ArrayList<>(Arrays.asList(
-                        x, y, -target));
-                Collections.sort(list);
-                result.add(list);
-            }
-            else {
-                set.add(y);
+    boolean isSolutionValid(Pair<Integer, Integer>[] pairs) {
+        HashMap<Integer, Integer> actual = new HashMap<>();
+        for (Pair<Integer, Integer> pair: pairs) {
+            if (actual.containsKey(pair.getKey())) actual.put(pair.getKey(), actual.get(pair.getKey()) + 1);
+            else actual.put(pair.getKey(), 1);
+        }
+
+        HashMap<Integer, Integer> real = new HashMap<>();
+        for (Pair<Integer, Integer> pair: pairs) {
+            if (!real.containsKey(pair.getKey()))
+                real.put(pair.getKey(), pair.getValue());
+        }
+
+        boolean valid = true;
+        for (Map.Entry<Integer, Integer> entry : actual.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = real.get(key);
+            if (entry.getValue() > value) {
+                valid = false;
+                break;
             }
         }
 
-        return result;
+        return valid;
     }
 
     List<List<Integer>> threeSum(int[] nums) {
         HashSet< List<Integer> > result = new HashSet<>();
-        for (int i = 0; i < nums.length; ++i) {
-            result.addAll(twoSum(nums, i));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums) {
+            if (map.containsKey(num)) map.put(num, map.get(num) + 1);
+            else map.put(num, 1);
+        }
+
+        for(Map.Entry<Integer, Integer> pair : map.entrySet()) {
+            for(Map.Entry<Integer, Integer> pair2 : map.entrySet()) {
+                int y = -pair.getKey() - pair2.getKey();
+                if (map.containsKey(y))
+                {
+                    Pair<Integer, Integer>[] solution = new Pair[3];
+                    solution[0] = new Pair<> ( pair.getKey(), pair.getValue() );
+                    solution[1] = new Pair<> ( pair2.getKey(), pair.getValue() );
+                    solution[2] = new Pair<> ( y, map.get(y) );
+                    if (isSolutionValid(solution)) {
+                        List<Integer> solutionList = Arrays.asList(
+                                pair.getKey(), pair2.getKey(), y);
+                        Collections.sort(solutionList);
+                        result.add(solutionList);
+                    }
+                }
+            }
         }
 
         return new ArrayList<>(result);
